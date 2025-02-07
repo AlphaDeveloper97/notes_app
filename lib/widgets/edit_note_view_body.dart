ecaac4e4e14af2c2ad846d8997ac1e9b6bbfcd41
Mostafa Ragab <mostafaragabelmshad@gmail.com>
2025-02-07 10:58:41 +0200

    add custom pp bar to edit note view

--- lib/widgets/edit_note_view_body.dart
+++ lib/widgets/edit_note_view_body.dart
@@ -1,16 +1,25 @@
 import 'package:flutter/material.dart';
+
 import 'package:note_app/widgets/custom_app_bar.dart';
-import 'package:note_app/widgets/custom_text_field.dart';
 
 class EditNoteViewBody extends StatelessWidget {
   const EditNoteViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Column(
-      children: [
-        CustomAppBar(),
-      ],
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 24),
+      child: const Column(
+        children: [
+          SizedBox(
+            height: 40,
+          ),
+          CustomAppBar(
+            title: 'Edit Note',
+            icon: Icons.check,
+          ),
+        ],
+      ),
     );
   }
 }
