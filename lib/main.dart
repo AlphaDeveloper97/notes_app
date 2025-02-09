9d57f447ddc00a68b2c30a4d6867fdf563ae7baf
Mostafa Ragab <mostafaragabelmshad@gmail.com>
2025-02-09 04:53:51 +0200

    init hive and open notes box

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:hive/hive.dart';
 
-import 'package:hive_flutter/adapters.dart';
+// import 'package:hive_flutter/adapters.dart';
+import 'package:note_app/constains.dart';
 
 import 'package:note_app/views/notes_view.dart';
+import 'package:path_provider/path_provider.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Hive.initFlutter();
+  // await Hive.initFlutter();
+  final appDocumentDir = await getApplicationDocumentsDirectory();
+  Hive.init(appDocumentDir.path);
 
+  await Hive.openBox(kNotesBox);
   runApp(const NotesApp());
 }
 
